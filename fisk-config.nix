2d765c3be24bf5c801b5c88232615ebac02947ce
hey_adora <146812294+hey-adora@users.noreply.github.com>
2025-05-18 00:28:12 +0300

    test

---
+++ fisk-config.nix
@@ -0,0 +1,71 @@
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/vda";
+        content = {
+          type = "gpt";
+          partitions = {
+            MBR = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+              priority = 1; # Needs to be first partition
+            };
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypted";
+                settings = {
+                  allowDiscards = true;
+                };
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ "-f" ];
+                  subvolumes = {
+                    root = {
+                      mountpoint = "/";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    home = {
+                      mountpoint = "/home";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    nix = {
+                      mountpoint = "/nix";
+                      mountOptions = [
+                        "compress=zstd"
+                        "noatime"
+                      ];
+                    };
+                    swap = {
+                      mountpoint = "/.swapvol";
+                      swap.swapfile.size = "20M";
+                    };
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 lsblk
 echo "select /dev/<disk>: "
-read $disk
-sudo mount -o remount,size=10G,noatime /nix/.rw-store
-rm -r /tmp/config
-nixos-generate-config --root /tmp/config --no-filesystems
-rm /tmp/config/etc/nixos/configuration.nix 
-cp configuration.nix /tmp/config/etc/nixos/configuration.nix
-cp flake.nix /tmp/config/etc/nixos/flake.nix
-sudo nix --experimental-features "nix-command flakes" run 'github:nix-community/disko/latest#disko-install' -- --write-efi-boot-entries --flake '/tmp/config/etc/nixos#adora' --disk main /dev/$disk --extra-files /tmp/config/etc/nixos/flake.nix /etc/nixos/flake.nix --extra-files /tmp/config/etc/nixos/configuration.nix /etc/nixos/configuration.nix --extra-files /tmp/config/etc/nixos/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
+# read $disk
+# sudo mount -o remount,size=10G,noatime /nix/.rw-store
+# rm -r /tmp/config
+# nixos-generate-config --root /tmp/config --no-filesystems
+# rm /tmp/config/etc/nixos/configuration.nix 
+# cp configuration.nix /tmp/config/etc/nixos/configuration.nix
+# cp flake.nix /tmp/config/etc/nixos/flake.nix
+# sudo nix --experimental-features "nix-command flakes" run 'github:nix-community/disko/latest#disko-install' -- --write-efi-boot-entries --flake '/tmp/config/etc/nixos#adora' --disk main /dev/$disk --extra-files /tmp/config/etc/nixos/flake.nix /etc/nixos/flake.nix --extra-files /tmp/config/etc/nixos/configuration.nix /etc/nixos/configuration.nix --extra-files /tmp/config/etc/nixos/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount ./disk-config.nix --disk main /dev/$disk
