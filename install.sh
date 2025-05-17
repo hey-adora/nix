21d9b6d82c7030347926d956696a5ba8ff086375
hey_adora <146812294+hey-adora@users.noreply.github.com>
2025-05-17 21:51:00 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,11 @@
 sudo mount -o remount,size=10G,noatime /nix/.rw-store
 nixos-generate-config --root . --no-filesystems
 rm ./etc/nixos/configuration.nix 
-mv ./etc/nixos/hardware-configuration.nix ./hardware-configuration.nix
-rmdir ./etc/nixos
-rmdir ./etc
+cp configuration.nix ./etc/nixos/configuration.nix
+cp flake.nix ./etc/nixos/flake.nix
+mv ./etc /tmp/config/etc
+# mv ./etc/nixos/hardware-configuration.nix ./hardware-configuration.nix
+# rmdir ./etc/nixos
+# rmdir ./etc
 #sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount ./disk.nix
 sudo nix --experimental-features "nix-command flakes" run 'github:nix-community/disko/latest#disko-install' -- --flake '.#mymachine' --disk main /dev/vda
