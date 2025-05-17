6a6b3996964c206ac82cfda47276c3ffb3b77198
hey_adora <146812294+hey-adora@users.noreply.github.com>
2025-05-18 00:56:54 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
   inputs.home-manager.url = github:nix-community/home-manager;
 
-  outputs = { self, nixpkgs, ... }@attrs: {
+  outputs = { self, nixpkgs, home-manager, ... }@attrs: {
     nixosConfigurations.fnord = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = attrs;
