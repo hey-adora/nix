c7e6e7c1286bb0daacdf3d7fa166620df7848838
hey_adora <146812294+hey-adora@users.noreply.github.com>
2025-05-17 14:34:17 +0300

    Update disk.nix

--- disk.nix
+++ disk.nix
@@ -7,12 +7,10 @@
         content = {
           type = "gpt";
           partitions = {
-            type = "gpt";
-            partitions = {
-              boot = {
-                size = "1M";
-                type = "EF02"; # for grub MBR
-              };
+            boot = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+            };
             ESP = {
               size = "512M";
               type = "EF00";
