b48f3a6448491c61390a3b42ed02c32917c3fa2a
hey_adora <146812294+hey-adora@users.noreply.github.com>
2025-05-17 22:19:34 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 sudo mount -o remount,size=10G,noatime /nix/.rw-store
-mkdir /tmp/config
-nixos-generate-config --root . --no-filesystems
-rm ./etc/nixos/configuration.nix 
-cp configuration.nix ./etc/nixos/configuration.nix
-cp flake.nix ./etc/nixos/flake.nix
-mv ./etc /tmp/config/etc
+nixos-generate-config --root /tmp/config --no-filesystems
+rm /tmp/config/etc/nixos/configuration.nix 
+cp configuration.nix /tmp/config/etc/nixos/configuration.nix
+cp flake.nix /tmp/config/etc/nixos/flake.nix
+# mv ./etc /tmp/config/etc
 # mv ./etc/nixos/hardware-configuration.nix ./hardware-configuration.nix
 # rmdir ./etc/nixos
 # rmdir ./etc
