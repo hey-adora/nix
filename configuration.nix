d49b9af087e4aa035256dfa7dacaa21fb8c481ba
hey_adora <146812294+hey-adora@users.noreply.github.com>
2025-05-18 01:46:32 +0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,7 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, nixpkgs, ... }: {
+{ config, lib, nixpkgs, home-manager, ... }: {
   # do something with home-manager here, for instance:
   imports = [ home-manager.nixosModules.default ./hardware-configuration.nix ];
 
