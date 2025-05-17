a4a69e5c7a96b3a22f2a057da937338e9e1946ba
hey_adora <146812294+hey-adora@users.noreply.github.com>
2025-05-17 16:01:12 +0300

    Update format.sh

--- format.sh
+++ format.sh
@@ -7,4 +7,4 @@ mv ./etc/nixos/hardware-configuration.nix ./hardware-configuration.nix
 rmdir ./etc/nixos
 rmdir ./etc
 #sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount ./disk.nix
-sudo nix --experimental-features "nix-command flakes" run 'github:nix-community/disko/latest#disko-install' -- --flake './flake.nix#adora' --disk main /dev/vda
+sudo nix --experimental-features "nix-command flakes" run 'github:nix-community/disko/latest#disko-install' -- --flake '.#adora' --disk main /dev/vda
