53a27349190499af429a90b37b5974df284f7e25
hey_adora <146812294+hey-adora@users.noreply.github.com>
2025-05-17 14:45:22 +0300

    Update format.sh

--- format.sh
+++ format.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sudo -o remount,size=10G,noatime /nix/.rw-store
+sudo mount -o remount,size=10G,noatime /nix/.rw-store
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount ./disk.nix
