730c05612450eadddf2ddb48428cdf9cb8215477
hey_adora <146812294+hey-adora@users.noreply.github.com>
2025-05-18 10:31:15 +0300

    home

--- flake.nix
+++ flake.nix
@@ -1,35 +1,31 @@
-# {
-#   inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-#   inputs.disko.url = "github:nix-community/disko/latest";
-#   inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
-
-#   outputs = { self,  disko, nixpkgs }: {
-#     nixosConfigurations.adora = nixpkgs.legacyPackages.x86_64-linux.nixos [
-#       ./configuration.nix
-#       disko.nixosModules.disko
-#     ];
-#   };
-# }
 {
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
   inputs.home-manager.url = github:nix-community/home-manager;
   inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.zen-browser.url = "github:0xc000022070/zen-browser-flake";
 
-  outputs = { self, nixpkgs, home-manager, ... }@ inputs: let
-    inherit (self) outputs; in {
+  outputs = { self, nixpkgs, home-manager, zen-browser, ... }@inputs: {
+    # FIXME change hostname
     nixosConfigurations.adora = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      specialArgs = {inherit inputs outputs };
-      modules = [ ./configuration.nix ];
-    };
-    homeConfigurations = {
-      # FIXME replace with your username@hostname
-      "hey@adora" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs outputs;};
-        # > Our main home-manager configuration file <
-        modules = [./home.nix];
-      };
+      specialArgs = {inherit inputs };
+      modules = [ 
+          ./configuration.nix
+          home-manager.nixosModules.home-manager 
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            # FIXME change username
+            home-manager.users.hey = import ./home.nix;
+            home-manager.extraSpecialArgs = {
+            
+              # bring in the list of inputs into the home-manager module
+              inherit inputs;
+              system = "x86_64-linux";
+            };
+          }
+        ];
     };
   };
 }
--- home.nix
+++ home.nix
@@ -5,44 +5,13 @@
   lib,
   config,
   pkgs,
+  system,
   ...
 }: {
-  # You can import other home-manager modules here
-  imports = [
-    # If you want to use home-manager modules from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModule
-
-    # You can also split up your configuration and import pieces of it here:
-    # ./nvim.nix
-  ];
-
-  nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    # Configure your nixpkgs instance
-    config = {
-      # Disable if you don't want unfree packages
-      allowUnfree = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/2942
-      allowUnfreePredicate = _: true;
-    };
-  };
 
   # TODO: Set your username
-  home = {
-    username = "hey";
-    homeDirectory = "/home/hey";
-  };
+  home.username = "hey";
+  home.homeDirectory = "/home/hey";
 
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
@@ -56,5 +25,5 @@
   systemd.user.startServices = "sd-switch";
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "24.11";
+  home.stateVersion = "25.05";
 }
