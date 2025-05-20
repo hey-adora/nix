0e0e190f966d42fcaf3aabe7281449f32b73e5c6
hey <146812294+hey-adora@users.noreply.github.com>
2025-05-20 12:41:37 +0300

    Update disk-config_2.nix

--- disk-config_2.nix
+++ disk-config_2.nix
@@ -3,23 +3,28 @@
     disk = {
       main = {
         type = "disk";
-        device = "/dev/disk/by-diskseq/1";
+        device = "/dev/vda";
         content = {
           type = "gpt";
           partitions = {
-            ESP = {
-              priority = 1;
-              name = "ESP";
-              start = "1M";
-              end = "1G";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
-              };
+            MBR = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+              priority = 1; # Needs to be first partition
             };
+            # ESP = {
+            #   priority = 1;
+            #   name = "ESP";
+            #   start = "1M";
+            #   end = "1G";
+            #   type = "EF00";
+            #   content = {
+            #     type = "filesystem";
+            #     format = "vfat";
+            #     mountpoint = "/boot";
+            #     mountOptions = [ "umask=0077" ];
+            #   };
+            # };
             root = {
               size = "100%";
               content = {
