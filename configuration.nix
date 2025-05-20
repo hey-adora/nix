daab274ef11f9aef440aec303a4c26efee896f37
hey <146812294+hey-adora@users.noreply.github.com>
2025-05-20 08:45:18 +0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -2,13 +2,18 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
 
   nix.settings.experimental-features = "nix-command flakes";
   nix.settings.auto-optimise-store = true;
@@ -26,9 +31,16 @@
 
   boot.initrd.secrets = {
     "/boot/crypto_keyfile.bin" = null;
+    "/boot/hddkey" = null;
   };
 
   boot.initrd.luks.devices.crypted.keyFile = "/boot/crypto_keyfile.bin";
+  boot.initrd.luks.devices.hdd3.keyFile = "/boot/crypto_keyfile.bin";
+  boot.initrd.luks.devices.hdd1.keyFile = "/boot/hddkey";
+  boot.initrd.luks.devices.hdd2.keyFile = "/boot/hddkey";
+
+  fileSystems."/mnt/hdd1".depends = [ "/" ];
+  #fileSystems."/mnt/hdd1".pass = 2;
 
   networking.hostName = "adora"; # Define your hostname.
   # Pick only one of the below networking options.
@@ -53,12 +65,10 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
   services.gnome.gnome-browser-connector.enable = true;
-  
 
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
@@ -84,9 +94,9 @@
     openssh.authorizedKeys.keys = [
       # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
     ];
-    extraGroups = ["wheel"];
+    extraGroups = [ "wheel" ];
     packages = with pkgs; [
-        home-manager
+      home-manager
     ];
   };
   # Define a user account. Don't forget to set a password with ‘passwd’.
@@ -112,12 +122,12 @@
     wl-clipboard
   ];
 
-  environment.etc.crypttab.mode = "0600";
-  environment.etc.crypttab.text = ''
-    hdd3           UUID=3466d7cf-6769-43e2-9293-2080eca0cd47    /boot/crypto_keyfile.bin
-    hdd1           UUID=a45f1e82-bf48-4b38-8481-84a442cfa31f    /boot/hddkey
-    hdd2           UUID=9b46e655-a653-4683-8cfa-d21c5e08fdaf    /boot/hddkey
-  '';
+  # environment.etc.crypttab.mode = "0600";
+  # environment.etc.crypttab.text = ''
+  #   hdd3           UUID=3466d7cf-6769-43e2-9293-2080eca0cd47    /boot/crypto_keyfile.bin
+  #   hdd1           UUID=a45f1e82-bf48-4b38-8481-84a442cfa31f    /boot/hddkey
+  #   hdd2           UUID=9b46e655-a653-4683-8cfa-d21c5e08fdaf    /boot/hddkey
+  # '';
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -163,4 +173,3 @@
   system.stateVersion = "25.11"; # Did you read the comment?
 
 }
-
